0d313bc2a5606c41cfba8b6f3b0a75329568270d
Daniel Saxton <daniel.saxton@gmail.com>
2018-08-31 20:56:35 -0500

    add empty regressor class

---
+++ ordreg/ordinal.py
@@ -0,0 +1,18 @@
+from sklearn.base import BaseEstimator, LinearClassifierMixin
+
+
+class OrdinalRegression(BaseEstimator, LinearClassifierMixin):
+    def __init__(self):
+        pass
+
+    def fit(self, X, y):
+        pass
+
+    def predict_proba(self, X):
+        pass
+
+    def predict_log_proba(self, X):
+        pass
+
+    def predict(self, X):
+        pass
